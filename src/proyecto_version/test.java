901394b71ed7218a0b15d0e540300901438993b5
1-DAM <1-DAM@Aula2-PC18>
2014-02-04 08:47:03 +0100

    Version 1.1 - Añadido el primer imprimir nombre

---
+++ src/proyecto_version/test.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto_version;
+
+/**
+ *
+ * @author 1-DAM
+ */
+public class test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola");
+    }
+}
