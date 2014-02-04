0d3afc30ddb26e6e090d25e257747a5154aec7ed
1-DAM <1-DAM@Aula2-PC18>
2014-02-04 09:16:23 +0100

    Version 1.1 - Añadido el primer imprimir nombre Version 1.2 - Añadido el segundo imrpimir nombre Version 1.3 - Añadido otro imprimir Version 2

--- src/proyecto_version/test.java
+++ src/proyecto_version/test.java
@@ -1,18 +1,10 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package proyecto_version;
 
-/**
- *
- * @author 1-DAM
- */
+
 public class test {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         System.out.println("Hola");
         System.out.println("Angel");
